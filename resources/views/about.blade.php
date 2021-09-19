c6ef9c012544b9d8ddc09655adb6c445d8bdbf3d
TegarDv <73943961+TegarDv@users.noreply.github.com>
2021-09-19 16:22:46 +0700

    Penggunaan Boostrap

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>About Us</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">                                      <!-- Bootstrap style -->
+        <link rel="stylesheet" href="css/templatemo-style.css">                                   <!-- Templatemo style -->
+    </head>
+    <body>
+        <h1 align=center>About Me</h1>
+        <p>
+            Nama    : Tegar Dwi Vantoro
+            kelas   : MI2E
+            No. Abs : 27
+        </p>
+    </body>
+</html>
